d77e1f0b391d282d2861792d59180d563e24543c
Jim Lightfoot <jim.lightfoot@outlook.com>
2025-05-18 15:45:08 -0700

    Add update method that takes lambda. Fix unit tests

---
+++ MondoCore.Azure.Cosmos.UnitTests/CosmosDB.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Azure.Cosmos;
+using MondoCore.Data;
+using MondoCore.Repository.TestHelper;
+
+namespace MondoCore.Azure.Cosmos.UnitTests
+{
+    [TestClass]
+    public sealed class CosmosDBTests : RepositoryTestBase
+    {
+        // This is for emulator
+        private const string _accountEndpoint        = "https://localhost:8081/";
+        private const string _authKeyOrResourceToken = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+       
+        public CosmosDBTests() :
+
+           base(new CosmosDB("testdb", _accountEndpoint, _authKeyOrResourceToken),
+                "testcontainer",
+                ()=> Guid.NewGuid().ToString())
+        {
+        }
+    }
+}
---
+++ MondoCore.Azure.Cosmos/CosmosContainer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+
+using MondoCore.Common;
+using MondoCore.Data;
+
+namespace MondoCore.Azure.Cosmos
+{
+    internal abstract class CosmosContainer<TID> 
+    {
+        private readonly IIdentifierStrategy<TID> _idStrategy;
+
+        internal CosmosContainer(Container container, IIdentifierStrategy<TID> strategy)
+        {
+            this.Container = container;
+            _idStrategy = strategy;
+        }
+
+        internal protected Container Container { get; }
+
+        internal protected (string Id, PartitionKey PartitionKey) SplitId(TID id)
+        {
+            var sid = "";
+            var partitionKey = PartitionKey.Null;
+
+            if(id is IPartitionedId partitionedId)
+            {
+                sid = partitionedId.Id;
+    
+                if(!string.IsNullOrWhiteSpace(partitionedId.PartitionKey))
+                { 
+                    partitionKey = new PartitionKey(partitionedId.PartitionKey);
+
+                    return (sid, partitionKey);
+                }
+            }
+
+            if(_idStrategy != null)
+            { 
+                var idResult = _idStrategy.GetId(id);
+                
+                if(string.IsNullOrWhiteSpace(sid))
+                    sid = idResult.Id;
+
+                if(partitionKey == PartitionKey.Null)
+                    partitionKey = new PartitionKey(idResult.PartitionKey);
+            }
+
+            return (id.ToString(), partitionKey);
+        }
+     
+        protected async Task<TValue> InternalGet<TValue>(string id, PartitionKey partitionKey)
+        {
+            try
+            { 
+                var result = await this.Container.ReadItemAsync<TValue>(id, partitionKey);
+            
+                if(result == null)
+                    throw new NotFoundException();
+
+                return result;
+            }
+            catch(CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                throw new NotFoundException();
+            }
+        }
+
+        protected IAsyncEnumerable<TValue> InternalGet<TValue>(Expression<Func<TValue, bool>> query)
+        {
+            return this.Container.GetItemLinqQueryable<TValue>()
+                                 .Where(query)
+                                 .ToFeedIterator()
+                                 .ToAsyncEnumerable<TValue>();
+        }
+        
+        protected string GetId<TValue>(TValue item)
+        {
+            if(item is IIdentifiable<TID> identifiable)
+                return identifiable.Id.ToString();
+
+            var id = item.GetValue<TID>("Id");
+
+            return id.ToString();
+        }
+    }
+
+    internal static class Extensions
+    {
+        internal static async IAsyncEnumerable<TModel> ToAsyncEnumerable<TModel>(this FeedIterator<TModel> setIterator)
+        {
+            while (setIterator.HasMoreResults)
+            { 
+                foreach (var item in await setIterator.ReadNextAsync())
+                {
+                    yield return item;
+                }
+            }
+
+            setIterator.Dispose();
+        }
+    }
+}
